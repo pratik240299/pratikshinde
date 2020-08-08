2405606ec58f15331c4ce4c29f7f5a0e79bf2938
pratik240299 <pratikshindeb67@gmail.com>
2020-08-08 13:32:58 +0530

    array

---
+++ cpractice/array/code2.c
@@ -0,0 +1,22 @@
+
+#include<stdio.h>
+
+ void main() {
+ 
+	 int jerNo[5] = {18,7,45,12,10};
+	 float avg[4] = {49.50,54.30,45.50,47.54};
+
+	 int run[5];
+
+	 printf("%ld\n",sizeof(jerNo));		// 20
+	 printf("%ld\n",sizeof(avg));    	 //16
+	 printf("%ld\n",sizeof(run)); 	// 20
+
+ }
+
+/*
+20
+16
+20
+
+*/
---
+++ cpractice/array/code3.c
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+
+ void main() {
+ 
+	 int jerNo[5] = {18,7,45,12,10};
+	 float avg[4] = {49.50,54.30,45.50,47.54};
+
+	 int run[5];
+
+	 printf("%d\n",jerNo[3]);		// 12
+	 printf("%f\n",avg[2]);    	 //45.500000
+	 printf("%d\n",run[4]); 	// garbage value
+
+ }
